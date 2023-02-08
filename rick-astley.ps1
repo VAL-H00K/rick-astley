c135a82ff4aba551e7e1f7e1fa059620cb82d88c
VAL-H00K <122081652+VAL-H00K@users.noreply.github.com>
2023-02-08 08:58:46 +0100

    Update rick-astley.ps1

--- rick-astley.ps1
+++ rick-astley.ps1
@@ -1,2 +1,4 @@
 # Turns Volume = 100%
 $k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+# Opens Rick Roll
+Start-Process "https://youtu.be/O91DT1pR1ew"
