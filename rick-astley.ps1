8fce185a0229d7e9140df3c5a03ff54a0a484f95
VAL-H00K <122081652+VAL-H00K@users.noreply.github.com>
2023-02-08 08:42:45 +0100

    Create rick-astley.ps1

---
+++ rick-astley.ps1
@@ -0,0 +1,3 @@
+# Turns Volume = 100%
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+STRING powershell.exe -e UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgACIAaAB0AHQAcABzADoALwAvAHcAdwB3AC4AeQBvAHUAdAB1AGIAZQAuAGMAbwBtAC8ALgAuAC8ALgAuAC8AdwBhAHQAYwBoAD8AdgA9AGQAUQB3ADQAdwA5AFcAZwBYAGMAUQAmADEAPQAxACIA
